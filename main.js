91b249bff23a896cf4f61c3323c077fc59b5132b
muli reuben <mulireuben57@gmail.com>
2024-03-05 09:21:49 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,108 @@
+// console.log("helo main from js")
+
+// let age=25
+// console.log(age)
+
+
+
+//data types
+
+
+// const person={
+//     "firstname":"reuben",
+//     "lastname":"muli"
+
+// }
+// console.log(person.firstname)
+// console.log(person.lastname)
+
+// const oddnumbers=[0,1,2,3,6,8,9]
+// console.log(oddnumbers[4])
+
+
+//arithmetic operators
+
+//  let a=10
+//  let y=5
+
+//  console.log(a+y)
+// console.log(++y)
+
+// //comparison operators
+
+// console.log(a == y)
+// console.log(a !==y)
+// console.log(a<y)
+// const isValidNumber =a >20  || 8>y
+// console.log(isValidNumber)
+
+// const isValid=false
+// console.log(!isValid)
+
+// console.log("bruce "+"melody")
+
+// const isEven=10 % 2 === 0 ? 'number is even' : 'number is odd'
+// console.log(isEven)
+
+//type conversions
+//implicit conversion
+
+//  convert number into  a string
+
+//console.log(Number("5"))
+// console.log(parseInt("5"))
+
+// console.log((500).toString())
+
+// const var1=10
+// const var2="10"
+
+// console.log(var1==var2)
+// console.log(var1===var2)
+
+//conditional statement
+//if else else if switch
+
+// const num=0
+// if(num >0){
+//     console.log("number is positive")
+// }
+// else if(num <0){
+//     console.log('number is negative')
+
+// }
+// else {
+//     console.log("number is zero")
+// }
+
+//for loop
+// for (let i=1;i<10;i++){
+//     console.log(i)
+// }
+
+
+// let i=1
+//     while (i<=5){
+//     console.log(i)
+//     i++}
+
+// function add(a,b){
+//     return a+b
+// }
+// const sum =add(10,20)
+// console.log(sum)
+
+//the arrow function
+
+// 
+//given a certain array
+
+// const material=["hydrogen","helium","lithium","brylium"]
+
+// //i want to use the arrow function to map the length of the items
+
+// console.log(material.map((material)=>material.length));
+ 
+
+
+
