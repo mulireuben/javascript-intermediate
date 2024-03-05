8da744c0a716bc2cfedadaf710b0f2272de2a94f
muli reuben <mulireuben57@gmail.com>
2024-03-05 15:14:31 +0300

    added some assync and wait functions

---
+++ array.js
@@ -0,0 +1,25 @@
+let myArray= [
+    {name:"rEUBEN",paidAll:true, balance:0},
+    {name:"Paul",paidAll:false, balance:100},
+    {name:"Mwangi",paidAll:true, balance:0},
+    {name:"Kimani",paidAll:false, balance:300},
+    {name:"Eick",paidAll:false, balance:500},
+    {name:"Ana",paidAll:true, balance:0},
+    {name:"Moses",paidAll:false, balance:790},
+    {name:"Roba",paidAll:true, balance:8000},
+    
+]
+
+// let unpaidpatients =myArray.filter((item)=> item.paidAll===false )
+
+// console.log(unpaidpatients.length
+//     )
+
+// let balancetotal=myArray.reduce((sum,item)=> (sum += item.balance),0)
+
+// console.log(balancetotal)
+// let paidAll=myArray.map((item)=>({
+//     ...item,
+//     paidAll:true
+// }))
+// console.log(paidAll)
